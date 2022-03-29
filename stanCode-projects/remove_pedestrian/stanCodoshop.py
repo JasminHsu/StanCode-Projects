82a12fbf3a659159cc90402661bce332b9e5983a
Chien-Chu (Jasmin) Hsu <88154130+JasminHsu@users.noreply.github.com>
2022-03-29 03:58:08 -0500

    Add files via upload

---
+++ stanCode-projects/remove_pedestrian/stanCodoshop.py
@@ -0,0 +1,170 @@
+"""
+File: stanCodoshop.py
+----------------------------------------------
+SC101_Assignment3
+Adapted from Nick Parlante's
+Ghost assignment by Jerry Liao.
+
+-----------------------------------------------
+In order to remove passerby in the photos, check
+and find the most suitable pixel to piece together
+a new photo without people
+"""
+
+import os
+import sys
+from simpleimage import SimpleImage
+
+
+def get_pixel_dist(pixel, red, green, blue):
+    """
+    Returns the color distance between pixel and mean RGB value
+
+    Input:
+        pixel (Pixel): pixel with RGB values to be compared
+        red (int): average red value across all images
+        green (int): average green value across all images
+        blue (int): average blue value across all images
+
+    Returns:
+        dist (float): color distance between red, green, and blue pixel values
+
+    """
+    dist = ((red-pixel.red)**2 + (green-pixel.green)**2 + (blue-pixel.blue)**2)**0.5
+    return dist
+
+
+def get_average(pixels):
+    """
+    Given a list of pixels, finds the average red, blue, and green values
+
+    Input:
+        pixels (List[Pixel]): list of pixels to be averaged
+    Returns:
+        rgb (List[int]): list of average red, green, blue values across pixels respectively
+
+    Assumes you are returning in the order: [red, green, blue]
+
+    """
+    pixel_rgb = []
+    r_sum = 0
+    g_sum = 0
+    b_sum = 0
+    for pixel in pixels:
+        # Add up each value
+        r_sum += pixel.red
+        g_sum += pixel.green
+        b_sum += pixel.blue
+    # Calculate the average
+    pixel_rgb.append(r_sum // len(pixels))
+    pixel_rgb.append(g_sum // len(pixels))
+    pixel_rgb.append(b_sum // len(pixels))
+    return pixel_rgb
+
+
+def get_best_pixel(pixels):
+    """
+    Given a list of pixels, returns the pixel with the smallest
+    distance from the average red, green, and blue values across all pixels.
+
+    Input:
+        pixels (List[Pixel]): list of pixels to be averaged and compared
+    Returns:
+        best (Pixel): pixel closest to RGB averages
+
+    """
+    pass
+    shortest_dist = float('inf')  # Set a reference point for comparison
+    best_pixel = pixels[0]
+    for pixel in pixels:
+        avg_rgb = get_average(pixels)
+        dist = get_pixel_dist(pixel, avg_rgb[0], avg_rgb[1], avg_rgb[2])
+        # Compare each value
+        if dist < shortest_dist:
+            shortest_dist = dist
+            best_pixel = pixel
+    return best_pixel
+
+
+def solve(images):
+    """
+    Given a list of image objects, compute and display a Ghost solution image
+    based on these images. There will be at least 3 images and they will all
+    be the same size.
+
+    Input:
+        images (List[SimpleImage]): list of images to be processed
+    """
+    width = images[0].width
+    height = images[0].height
+    result = SimpleImage.blank(width, height)
+    ######## YOUR CODE STARTS HERE #########
+    # Write code to populate image and create the 'ghost' effect
+    # To check every pixel on the canvas
+    for x in range(result.width):
+        for y in range(result.height):
+            pixels = []
+            for image in images:
+                pixel = image.get_pixel(x, y)
+                pixels.append(pixel)
+            best_pixel = get_best_pixel(pixels)
+            # Assign the best pixel to blank canvas and get a new photo
+            result_pixel = result.get_pixel(x, y)
+            result_pixel.red = best_pixel.red
+            result_pixel.green = best_pixel.green
+            result_pixel.blue = best_pixel.blue
+    ######## YOUR CODE ENDS HERE ###########
+    print("Displaying image!")
+    result.show()
+
+
+def jpgs_in_dir(dir):
+    """
+    (provided, DO NOT MODIFY)
+    Given the name of a directory, returns a list of the .jpg filenames
+    within it.
+
+    Input:
+        dir (string): name of directory
+    Returns:
+        filenames(List[string]): names of jpg files in directory
+    """
+    filenames = []
+    for filename in os.listdir(dir):
+        if filename.endswith('.jpg'):
+            filenames.append(os.path.join(dir, filename))
+    return filenames
+
+
+def load_images(dir):
+    """
+    (provided, DO NOT MODIFY)
+    Given a directory name, reads all the .jpg files within it into memory and
+    returns them in a list. Prints the filenames out as it goes.
+
+    Input:
+        dir (string): name of directory
+    Returns:
+        images (List[SimpleImages]): list of images in directory
+    """
+    images = []
+    jpgs = jpgs_in_dir(dir)
+    for filename in jpgs:
+        print("Loading", filename)
+        image = SimpleImage(filename)
+        images.append(image)
+    return images
+
+
+def main():
+    # (provided, DO NOT MODIFY)
+    args = sys.argv[1:]
+    # We just take 1 argument, the folder containing all the images.
+    # The load_images() capability is provided above.
+    images = load_images(args[0])
+    # images = load_images("monster")
+    solve(images)
+
+
+if __name__ == '__main__':
+    main()
